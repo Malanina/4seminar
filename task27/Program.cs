40600dea6223b18f63d2837b6893a719da807863
Katerina <kate.malanina85@gmail.com>
2022-11-13 21:51:33 +0300

    Задача 27

--- task27/Program.cs
+++ task27/Program.cs
@@ -3,3 +3,16 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number = number / 10;
+    }
+    Console.WriteLine($"Сумма цифр в введенном числе: {sum}");
+
